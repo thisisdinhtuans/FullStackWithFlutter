c714e83b9b6d948ae1752a0a099e455fd462f956
Dinh Tuans <dinhmanhtuans89@gmail.com>
2023-12-13 19:58:27 +0700

    bo

--- FullStackWithFlutter-UI/fullstackwithflutter_ui/lib/screens/user_list_screen.dart
+++ FullStackWithFlutter-UI/fullstackwithflutter_ui/lib/screens/user_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:convert/convert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fullstackwithflutter_ui/providers/user_provider.dart';
@@ -57,22 +59,55 @@ class UsersListView extends ConsumerWidget {
                   itemCount: users.data.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pushNamed(
-                                UserViewScreen.routeName,
-                                arguments: users.data[index].id.toString());
-                          },
-                          child: ListTile(
-                            title: Text(users.data[index].fullName),
-                            subtitle:
-                                Text(users.data[index].mobileNumber.toString()),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pushNamed(
+                                  UserViewScreen.routeName,
+                                  arguments: users.data[index].id.toString());
+                            },
+                            child: ListTile(
+                              title: Text(users.data[index].fullName),
+                              subtitle: Text(
+                                  users.data[index].mobileNumber.toString()),
+                              trailing: SizedBox(
+                                width: 80,
+                                child: Row(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(3.0),
+                                      child: GestureDetector(
+                                        child: const Icon(Icons.edit,
+                                            color: Colors.purple),
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const UserFormScreen(),
+                                                settings: RouteSettings(
+                                                    name: "editPerson",
+                                                    arguments:
+                                                        (users.data[index]))),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(3.0),
+                                      child: GestureDetector(
+                                        child: const Icon(Icons.delete,
+                                            color: Colors.red),
+                                        onTap: () {},
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    );
+                        ));
                   }),
             ),
           );
--- FullStackWithFlutter-UI/fullstackwithflutter_ui/lib/screens/userview_screen.dart
+++ FullStackWithFlutter-UI/fullstackwithflutter_ui/lib/screens/userview_screen.dart
@@ -1,13 +1,29 @@
+import 'dart:js';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fullstackwithflutter_ui/models/user.dart';
 import 'package:fullstackwithflutter_ui/providers/user_provider.dart';
 
 class UserViewScreen extends ConsumerWidget {
   final Object? userObject;
   const UserViewScreen({super.key, required this.userObject});
   static const String routeName = "/user-view";
+
+  // @override
+  // void didChangeDependencies() {
+  //   // super.didChangeDependencies();
+  //   var personString = ModalRoute.of(context)?.settings.arguments as String;
+  //   print(personString);
+  // }
+  // @override
+  // void didChangeDependencies() {
+  //   var user =
+  //       ModalRoute.of(context as BuildContext)?.settings.arguments as User;
+  //   print(user);
+  // }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
